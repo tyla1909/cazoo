b8cb09ce807a0929134d0f30061686d9df93945a
Oluwatobi Peter Olowo <104519109+tyla1909@users.noreply.github.com>
2022-09-19 06:37:55 -0400

    first commit

---
+++ utils_lib/cazoo.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.cazoo.co.uk/cars/')
